5227d125b3a39c7fe7e9afa63fed1c1e467aed9b
jordangreenwebsite <jordan.greenwebsite@gmail.com>
2025-10-22 19:24:12 +0100

    Updated/Added simply-static-1-1761157082

---
+++ wp-content/plugins/advanced-custom-post-type/assets/build/assets/InputHidden-CbKoMq6o.js
@@ -0,0 +1,1 @@
+import{j as i}from"./fields-GHpMjljq.js";import{R as n}from"./vendor-BmsXziqF.js";import{P as e}from"./index-N_DirKM7.js";const p=({id:r,value:s,register:t})=>i.jsx(n.Fragment,{children:i.jsx("input",{id:r,name:r,type:"hidden",value:s||"",...t(r)})});p.propTypes={id:e.string.isRequired,value:e.oneOfType([e.string,e.number]).isRequired,register:e.func.isRequired};export{p as I};
